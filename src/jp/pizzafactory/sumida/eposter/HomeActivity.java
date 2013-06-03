c59b7bb6cda1e5891955c784349eb8a7fed24c66
Masaki Muranaka <monaka@monami-ya.jp>
2013-06-03 09:32:25 +0900

    Organize imports.

--- src/jp/pizzafactory/sumida/eposter/HomeActivity.java
+++ src/jp/pizzafactory/sumida/eposter/HomeActivity.java
@@ -18,8 +18,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
-import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 
