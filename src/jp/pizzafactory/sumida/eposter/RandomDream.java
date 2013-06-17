2da2dd07e05e7036b02f42a2fd9067df09bce02f
Masaki Muranaka <monaka@monami-ya.jp>
2013-06-17 17:10:52 +0900

    Support Preferences for DayDream.

--- src/jp/pizzafactory/sumida/eposter/RandomDream.java
+++ src/jp/pizzafactory/sumida/eposter/RandomDream.java
@@ -2,13 +2,16 @@ package jp.pizzafactory.sumida.eposter;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.service.dreams.DreamService;
 import android.support.v4.view.ViewPager;
 import android.view.MotionEvent;
@@ -18,7 +21,6 @@ import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 
 public class RandomDream extends DreamService {
-	private static final long TIMER_PERIOD = 3000;
 	private ViewPager mPager;
 	private ImagePagerAdapter adapter;
 
@@ -60,19 +62,42 @@ public class RandomDream extends DreamService {
 	@Override
 	public void onDreamingStarted() {
 		super.onDreamingStarted();
+
+		final SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(RandomDream.this);
+
+		int timerSpan = Integer.parseInt(sp.getString("span", "5000"));
 		timer = new Timer();
 		timer.schedule(new TimerTask() {
+			int[] hoursTable = new int[] { R.string.enable0, R.string.enable1,
+					R.string.enable2, R.string.enable3, R.string.enable4,
+					R.string.enable5, R.string.enable6, R.string.enable7,
+					R.string.enable8, R.string.enable9, R.string.enable10,
+					R.string.enable11, R.string.enable12, R.string.enable13,
+					R.string.enable14, R.string.enable15, R.string.enable16,
+					R.string.enable17, R.string.enable18, R.string.enable19,
+					R.string.enable20, R.string.enable21, R.string.enable22,
+					R.string.enable23 };
 
 			@Override
 			public void run() {
 				handler.post(new Runnable() {
 					public void run() {
-						mPager.setCurrentItem(mPager.getCurrentItem()
-								+ (int) (Math.random() * 11) - 5);
+						Calendar calendar = Calendar.getInstance();
+						String idString = getString(hoursTable[calendar
+								.get(Calendar.HOUR_OF_DAY)]);
+						int visibility = sp.getBoolean(idString, true) ? View.VISIBLE
+								: View.INVISIBLE;
+						findViewById(R.id.viewpager).setVisibility(visibility);
+						findViewById(R.id.home).setVisibility(visibility);
+						if (visibility == View.VISIBLE) {
+							mPager.setCurrentItem(mPager.getCurrentItem()
+									+ (int) (Math.random() * 7) - 3);
+						}
 					}
 				});
 			}
-		}, TIMER_PERIOD, TIMER_PERIOD);
+		}, timerSpan, timerSpan);
 	}
 
 	@Override
