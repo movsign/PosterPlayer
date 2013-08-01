3b5a973187ed34a6b365008bcb87f51b817e4e6f
Masaki Muranaka <monaka@monami-ya.jp>
2013-08-01 16:22:17 +0900

    Close ForcePortraitActivity by itself.
    It may avoid screen blackouts... possibly.

--- src/info/movsign/eposter/ForcePortraitActivity.java
+++ src/info/movsign/eposter/ForcePortraitActivity.java
@@ -1,6 +1,24 @@
 package info.movsign.eposter;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class ForcePortraitActivity extends Activity {
+	Handler handler;
+
+	@Override
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		handler = new Handler();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		handler.postDelayed(new Runnable () {
+			public void run() {
+				ForcePortraitActivity.this.finish();
+			}}, 3000);
+	}
 }
