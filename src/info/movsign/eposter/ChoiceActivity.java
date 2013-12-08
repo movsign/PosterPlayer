94295842173d7d9679cff1c2afc9569a893b88dc
Masaki Muranaka <monaka@monami-ya.jp>
2013-12-08 13:43:47 +0900

    Add touch-count logger database.

--- src/info/movsign/eposter/ChoiceActivity.java
+++ src/info/movsign/eposter/ChoiceActivity.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
@@ -21,59 +22,56 @@ import com.artifex.mupdfdemo.MuPDFReaderView;
 import com.artifex.mupdfdemo.MuPDFReflowAdapter;
 import com.artifex.mupdfdemo.OutlineActivityData;
 
-public class ChoiceActivity extends Activity
-{
-	private MuPDFCore    core;
+public class ChoiceActivity extends Activity {
+	private MuPDFCore core;
 	private MuPDFReaderView mDocView;
 	private boolean mReflow = false;
 
-	private MuPDFCore openFile(String path)
-	{
-		System.out.println("Trying to open "+path);
-		try
-		{
+	private MuPDFCore openFile(String path) {
+		System.out.println("Trying to open " + path);
+		try {
 			core = new MuPDFCore(this, path);
 			// New file: drop the old outline data
 			OutlineActivityData.set(null);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 		return core;
 	}
 
-	private MuPDFCore openBuffer(byte buffer[])
-	{
+	private MuPDFCore openBuffer(byte buffer[]) {
 		System.out.println("Trying to open byte buffer");
-		try
-		{
+		try {
 			core = new MuPDFCore(this, buffer);
 			// New file: drop the old outline data
 			OutlineActivityData.set(null);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 		return core;
+	}
+
+	private void insertLog(Uri uri) {
+		ContentValues values = new ContentValues();
+		values.put("uri", uri.toString());
+		getContentResolver().insert(
+				Uri.parse("content://info.movsign.eposter.touchcountprovider"),
+				values);
 	}
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		getWindow().addFlags(
 				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
 						| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
 						| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 		super.onCreate(savedInstanceState);
 
-
 		if (core == null) {
-			core = (MuPDFCore)getLastNonConfigurationInstance();
+			core = (MuPDFCore) getLastNonConfigurationInstance();
 		}
 		if (core == null) {
 			Intent intent = getIntent();
@@ -81,34 +79,38 @@ public class ChoiceActivity extends Activity
 			if (Intent.ACTION_VIEW.equals(intent.getAction())) {
 				Uri uri = intent.getData();
 				if (uri.toString().startsWith("content://")) {
-					// Handle view requests from the Transformer Prime's file manager
-					// Hopefully other file managers will use this same scheme, if not
+					// Handle view requests from the Transformer Prime's file
+					// manager
+					// Hopefully other file managers will use this same scheme,
+					// if not
 					// using explicit paths.
-					Cursor cursor = getContentResolver().query(uri, new String[]{"_data"}, null, null, null);
+					Cursor cursor = getContentResolver().query(uri,
+							new String[] { "_data" }, null, null, null);
 					if (cursor.moveToFirst()) {
 						String str = cursor.getString(0);
 						String reason = null;
 						if (str == null) {
 							try {
-								InputStream is = getContentResolver().openInputStream(uri);
+								InputStream is = getContentResolver()
+										.openInputStream(uri);
 								int len = is.available();
 								buffer = new byte[len];
 								is.read(buffer, 0, len);
 								is.close();
-							}
-							catch (java.lang.OutOfMemoryError e)
-							{
-								System.out.println("Out of memory during buffer reading");
+							} catch (java.lang.OutOfMemoryError e) {
+								System.out
+										.println("Out of memory during buffer reading");
+								reason = e.toString();
+							} catch (Exception e) {
 								reason = e.toString();
 							}
-							catch (Exception e) {
-								reason = e.toString();
-							}
-							if (reason != null)
-							{
+							if (reason != null) {
 								buffer = null;
 								Resources res = getResources();
-								setTitle(String.format(res.getString(R.string.cannot_open_document_Reason), reason));
+								setTitle(String
+										.format(res
+												.getString(R.string.cannot_open_document_Reason),
+												reason));
 								return;
 							}
 						} else {
@@ -123,13 +125,14 @@ public class ChoiceActivity extends Activity
 				}
 				if (core.countPages() == 0)
 					core = null;
+
+				insertLog(uri);
 			}
 			if (core != null && core.needsPassword()) {
 				return;
 			}
 		}
-		if (core == null)
-		{
+		if (core == null) {
 			return;
 		}
 
@@ -144,18 +147,17 @@ public class ChoiceActivity extends Activity
 		mDocView = (MuPDFReaderView) findViewById(R.id.choiceview);
 		// Now create the UI.
 		// First create the document view
-//		mDocView = new MuPDFReaderView(this);
+		// mDocView = new MuPDFReaderView(this);
 		mDocView.setAdapter(new MuPDFPageAdapter(this, core));
-
 
 		reflowModeSet(false);
 
 		// Stick the document view and the buttons overlay into a parent view
-//		RelativeLayout layout = new RelativeLayout(this);
-//		layout.addView(mDocView);
-//		layout.setBackgroundResource(R.drawable.tiled_background);
-		//layout.setBackgroundResource(R.color.canvas);
-//		setContentView(layout);
+		// RelativeLayout layout = new RelativeLayout(this);
+		// layout.addView(mDocView);
+		// layout.setBackgroundResource(R.drawable.tiled_background);
+		// layout.setBackgroundResource(R.color.canvas);
+		// setContentView(layout);
 		ImageView imageView = (ImageView) findViewById(R.id.home);
 		imageView.setOnClickListener(new OnClickListener() {
 
@@ -168,22 +170,20 @@ public class ChoiceActivity extends Activity
 		});
 	}
 
-	public Object onRetainNonConfigurationInstance()
-	{
+	public Object onRetainNonConfigurationInstance() {
 		MuPDFCore mycore = core;
 		core = null;
 		return mycore;
 	}
 
-	private void reflowModeSet(boolean reflow)
-	{
+	private void reflowModeSet(boolean reflow) {
 		mReflow = reflow;
-		mDocView.setAdapter(mReflow ? new MuPDFReflowAdapter(this, core) : new MuPDFPageAdapter(this, core));
+		mDocView.setAdapter(mReflow ? new MuPDFReflowAdapter(this, core)
+				: new MuPDFPageAdapter(this, core));
 		mDocView.refresh(mReflow);
 	}
 
-	public void onDestroy()
-	{
+	public void onDestroy() {
 		if (core != null)
 			core.onDestroy();
 		core = null;
@@ -192,8 +192,7 @@ public class ChoiceActivity extends Activity
 
 	@Override
 	protected void onStart() {
-		if (core != null)
-		{
+		if (core != null) {
 			core.startAlerts();
 		}
 
@@ -202,15 +201,16 @@ public class ChoiceActivity extends Activity
 		ImageView ivRight = (ImageView) findViewById(R.id.swipe_right);
 
 		if (core != null && core.countPages() != 1) {
-	//		ivLeft.setVisibility(View.VISIBLE);
-	//		AnimatorSet setLeft = (AnimatorSet) AnimatorInflater.loadAnimator(this,
-	//				R.animator.blink);
-	//		setLeft.setTarget(ivLeft);
-	//		setLeft.start();
-	
+			// ivLeft.setVisibility(View.VISIBLE);
+			// AnimatorSet setLeft = (AnimatorSet)
+			// AnimatorInflater.loadAnimator(this,
+			// R.animator.blink);
+			// setLeft.setTarget(ivLeft);
+			// setLeft.start();
+
 			ivRight.setVisibility(View.VISIBLE);
-			AnimatorSet setRight = (AnimatorSet) AnimatorInflater.loadAnimator(this,
-					R.animator.blink);
+			AnimatorSet setRight = (AnimatorSet) AnimatorInflater.loadAnimator(
+					this, R.animator.blink);
 			setRight.setTarget(ivRight);
 			setRight.start();
 		} else {
@@ -221,8 +221,7 @@ public class ChoiceActivity extends Activity
 
 	@Override
 	protected void onStop() {
-		if (core != null)
-		{
+		if (core != null) {
 			core.stopAlerts();
 		}
 
---
+++ src/info/movsign/eposter/TouchCountProvider.java
@@ -0,0 +1,87 @@
+package info.movsign.eposter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class TouchCountProvider extends ContentProvider {
+	TouchCountDatabaseHelper databaseHelper;
+	
+	@Override
+	public boolean onCreate() {
+	    databaseHelper = new TouchCountDatabaseHelper(getContext());
+	    return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+	    SQLiteDatabase db = databaseHelper.getReadableDatabase();
+	    SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+	    qb.setTables(TouchCountDatabaseHelper.table); //テーブル名
+	    Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, null);
+	    return c;		
+
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+	    SQLiteDatabase db = databaseHelper.getWritableDatabase();
+	 // setting the format to sql date time
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+	    Date date = new Date();
+	    values.put("datetime", dateFormat.format(date));
+	    db.insert(TouchCountDatabaseHelper.table, null, values);
+	    return null;
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	class TouchCountDatabaseHelper extends SQLiteOpenHelper {
+		static final String table = "touch";
+		
+		public TouchCountDatabaseHelper(Context context) {
+            super(context, "touchcount.db", null, 1);
+        }
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+	          db.execSQL("CREATE TABLE " + table + " ("
+	                    + BaseColumns._ID + " INTEGER PRIMARY KEY,"
+	                    + "filename TEXT,"
+	                    + "datetime TEXT"
+	                    + ");");			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		}
+		
+	}
+}
